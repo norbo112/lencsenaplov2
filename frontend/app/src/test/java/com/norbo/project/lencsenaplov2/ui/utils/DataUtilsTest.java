d37176ba845546d7547df47b68aa37cfa891d70b
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2020-06-21 14:30:51 +0200

    teszt és mockito hozzáadása

--- frontend/app/src/test/java/com/norbo/project/lencsenaplov2/ui/utils/DataUtilsTest.java
+++ frontend/app/src/test/java/com/norbo/project/lencsenaplov2/ui/utils/DataUtilsTest.java
@@ -2,19 +2,27 @@ package com.norbo.project.lencsenaplov2.ui.utils;
 
 import com.norbo.project.lencsenaplov2.ui.utilts.DataUtils;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneOffset;
 
 public class DataUtilsTest {
     private DataUtils underTest;
 
     @BeforeEach
-    void init() {
+    void setUp() {
         underTest = new DataUtils();
     }
 
     @Test
-    void elapsedTime_helyes_eltelt_ido_mutatasa() {
-
+    public void elapsedTimeFloat_helyes_eltelt_ido_mutatasa() {
+        long adatStart = LocalDateTime.of(2020, Month.JANUARY, 1, 12,00,00).toInstant(ZoneOffset.ofHours(2)).toEpochMilli();
+        long adatEnd = LocalDateTime.of(2020, Month.JANUARY, 1, 15,00,00).toInstant(ZoneOffset.ofHours(2)).toEpochMilli();
+        float actual = underTest.elapsedTimeFloat(adatStart, adatEnd);
+        Assertions.assertEquals(180, actual);
     }
 }
